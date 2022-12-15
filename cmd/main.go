dd9bc0abd6c6bf27628bc781dd66c134dc74fd76
Victor Milhomem <victormllosada@gmail.com>
2022-12-14 23:16:12 -0300

    players structure moved to package types

--- cmd/main.go
+++ cmd/main.go
@@ -7,104 +7,18 @@ import (
 	"time"
 
 	"github.com/VictorMilhomem/fbreScraper/cmd/colors"
+	"github.com/VictorMilhomem/fbreScraper/cmd/types"
 	"github.com/gocolly/colly"
 )
 
 // TODO: url for custom teams "https://fbref.com/en/squads/ID/YEAR/all_comps/TEAMNAME-Stats-All-Competitions"
 // TODO: maybe url for get comp tables
+var playerInfo types.PlayerBasic
 
-type ShootingStats struct {
-	gls       string `json:"gols"`
-	sht       string `json:"total_shots"`
-	sot       string `json:"shots_on_target"`
-	sot_per   string `json:"shots_on_target_per"`
-	sht_ft    string `json:"shots_fulltime"`
-	sot_ft    string `json:"shots_on_target_fulltime"`
-	gls_shot  string `json:"gols_per_shot"`
-	dist      string `json:"distance"`
-	fk        string `json:"free_kick"`
-	pk        string `json:"penalty_kick"`
-	pkAtt     string `json:"penalty_kick_attempted"`
-	xg        string `json:"xG"`
-	npxg      string `json:"npxG"`
-	npxg_shot string `json:"npxG_shot"`
-	gls_xg    string `json:"G-xG"`
-	np        string `json:"np:G-xG"`
-}
-
-type PassingStats struct{}
-
-type PassTypesStats struct{}
-
-type GoalShotCreationStats struct{}
-
-type DefenseStats struct{}
-
-type PossessionStats struct{}
-
-type PlayingTimeStats struct{}
-
-type MiscellaneousStats struct{}
-
-type PlayerSummaryStats struct{}
-
-type GoalKeepingStats struct {
-	goalAgainst                 string `json:"goal_against"`
-	penaltyKickAllowed          string `json:"pk_allowed"`
-	freeKickAllowed             string `json:"fk_allowed"`
-	cornerGoalsAgainst          string `json:"ck_allowed"`
-	ownGoals                    string `json:"own_goals"`
-	postShotxG                  string `json:"ps_xG"`
-	postShotxGSot               string `json:"ps_xG_SoT"`
-	postShotxGMinusGlsAllowed   string `json:"ps_xG_minus_gls_allowed"`
-	postShotxGMinusGlsAllowedFt string `json:"ps_xG_minus_gls_allowed_ft"`
-
-	// launched more than 40yards
-	launchedPassesCompletedLonger string `json:"passes_cmp_longer"`
-	launchedPassesAttemptedLonger string `json:"passes_att_longer"`
-	launchedPassesCompletedPer    string `json:"passes_cmp_per"`
-
-	// passes not including goal kick
-	passesAttempted string `json:"passes_att"`
-	throwsAttempted string `json:"thr_att"`
-	launchPer       string `json:"launch_per"`
-	averageLen      string `json:"avg_len"`
-
-	// goal kicks
-	gkAttempted  string `json:"gk_att"`
-	gkLaunchPer  string `json:"gk_launch_per"`
-	gkAverageLen string `json:"gk_avg_len"`
-
-	// Crosses
-	opponentAttemptedCrosses string `json:"crosses_opp"`
-	crossesStopedByKeeper    string `json:"crosses_stp_by_keeper"`
-	crossesStopedByKeeperPer string `json:"crosses_stp_by_keeper_per"`
-
-	// sweeper
-	nDefensiveActionsByKeeper   string `json:"n_defense_actions_by_keeper"`
-	nDefensiveActionsByKeeperFt string `json:"n_defense_actions_by_keeper_ft"`
-	averageDistance             string `json:"average_distance"`
-}
-
-// TODO: maybe create a interface for a player, to solve the problem where i need to get the stats from diff tables
-// TODO: create a struct to handle all players statistics
-type Player struct {
-	name             string                `json:"name"`
-	nation           string                `json:"nation"`
-	pos              string                `json:"position"`
-	age              string                `json:"age"`
-	min              string                `json:"minutes_played"`
-	shooting         ShootingStats         `json: "shooting_stats"`
-	passing          PassingStats          `json:"passing_stats"`
-	passType         PassTypesStats        `json:"passing_types"`
-	goalShotCreation GoalShotCreationStats `json:"goal_shot_creation"`
-	defenseActions   DefenseStats          `json:"defense_actions"`
-	possession       PossessionStats       `json:"possession"`
-	playingTime      PlayingTimeStats      `json:"playing_time"`
-	miscellaneous    MiscellaneousStats    `json:"miscellaneous"`
-	playerSummary    PlayerSummaryStats    `json:"player_summary"`
-	goalkeeping      GoalKeepingStats      `json:"goalkeeping"`
-}
+var (
+	shootingStats types.ShootingStats
+	player        types.Player
+)
 
 // TODO: Accept all teams stastistics and players by id and name ?
 
@@ -138,38 +52,45 @@ func main() {
 	})
 
 	// TODO: getting the struct field infos from each respective table
-	var players []Player
+
+	// Shooting statistics
+	var players []types.Player
 	c.OnHTML("table#stats_shooting_combined > tbody", func(h *colly.HTMLElement) {
 		h.ForEach("tr", func(i int, el *colly.HTMLElement) {
-			player := Player{
-				name:   el.ChildText("th"),
-				nation: el.ChildText("td:nth-child(1)"),
-				pos:    el.ChildText("td:nth-child(2)"),
-				age:    el.ChildText("td:nth-child(3)"),
-				min:    el.ChildText("td:nth-child(4)"),
-				shooting: ShootingStats{
-					gls:       el.ChildText("td:nth-child(5)"),
-					sht:       el.ChildText("td:nth-child(6)"),
-					sot:       el.ChildText("td:nth-child(7)"),
-					sot_per:   el.ChildText("td:nth-child(8)"),
-					sht_ft:    el.ChildText("td:nth-child(9)"),
-					sot_ft:    el.ChildText("td:nth-child(10)"),
-					gls_shot:  el.ChildText("td:nth-child(11)"),
-					dist:      el.ChildText("td:nth-child(12)"),
-					fk:        el.ChildText("td:nth-child(13)"),
-					pk:        el.ChildText("td:nth-child(14)"),
-					pkAtt:     el.ChildText("td:nth-child(15)"),
-					xg:        el.ChildText("td:nth-child(16)"),
-					npxg:      el.ChildText("td:nth-child(17)"),
-					npxg_shot: el.ChildText("td:nth-child(18)"),
-					gls_xg:    el.ChildText("td:nth-child(19)"),
-					np:        el.ChildText("td:nth-child(20)"),
-				},
+			playerInfo = types.PlayerBasic{
+				Name:   el.ChildText("th"),
+				Nation: el.ChildText("td:nth-child(1)"),
+				Pos:    el.ChildText("td:nth-child(2)"),
+				Age:    el.ChildText("td:nth-child(3)"),
+				Min:    el.ChildText("td:nth-child(4)"),
+			}
+
+			shootingStats = types.ShootingStats{
+				Gls:       el.ChildText("td:nth-child(5)"),
+				Sht:       el.ChildText("td:nth-child(6)"),
+				Sot:       el.ChildText("td:nth-child(7)"),
+				Sot_per:   el.ChildText("td:nth-child(8)"),
+				Sht_ft:    el.ChildText("td:nth-child(9)"),
+				Sot_ft:    el.ChildText("td:nth-child(10)"),
+				Gls_shot:  el.ChildText("td:nth-child(11)"),
+				Dist:      el.ChildText("td:nth-child(12)"),
+				Fk:        el.ChildText("td:nth-child(13)"),
+				Pk:        el.ChildText("td:nth-child(14)"),
+				PkAtt:     el.ChildText("td:nth-child(15)"),
+				Xg:        el.ChildText("td:nth-child(16)"),
+				Npxg:      el.ChildText("td:nth-child(17)"),
+				Npxg_shot: el.ChildText("td:nth-child(18)"),
+				Gls_xg:    el.ChildText("td:nth-child(19)"),
+				Np:        el.ChildText("td:nth-child(20)"),
 			}
 			// p, err := json.Marshal(player)
 			// if err != nil {
 			//     log.Fatal(err)
 			// }
+			player = types.Player{
+				PlayerBasicInfo: playerInfo,
+				Shooting:        shootingStats,
+			}
 			players = append(players, player)
 			log.Println(players)
 		})
--- cmd/types/player.go
+++ cmd/types/player.go
@@ -1,1 +1,98 @@
 package types
+
+type ShootingStats struct {
+	Gls       string `json:"gols"`
+	Sht       string `json:"total_shots"`
+	Sot       string `json:"shots_on_target"`
+	Sot_per   string `json:"shots_on_target_per"`
+	Sht_ft    string `json:"shots_fulltime"`
+	Sot_ft    string `json:"shots_on_target_fulltime"`
+	Gls_shot  string `json:"gols_per_shot"`
+	Dist      string `json:"distance"`
+	Fk        string `json:"free_kick"`
+	Pk        string `json:"penalty_kick"`
+	PkAtt     string `json:"penalty_kick_attempted"`
+	Xg        string `json:"xG"`
+	Npxg      string `json:"npxG"`
+	Npxg_shot string `json:"npxG_shot"`
+	Gls_xg    string `json:"G-xG"`
+	Np        string `json:"np:G-xG"`
+}
+
+type PassingStats struct{}
+
+type PassTypesStats struct{}
+
+type GoalShotCreationStats struct{}
+
+type DefenseStats struct{}
+
+type PossessionStats struct{}
+
+type PlayingTimeStats struct{}
+
+type MiscellaneousStats struct{}
+
+type PlayerSummaryStats struct{}
+
+type GoalKeepingStats struct {
+	GoalAgainst                 string `json:"goal_against"`
+	PenaltyKickAllowed          string `json:"pk_allowed"`
+	FreeKickAllowed             string `json:"fk_allowed"`
+	CornerGoalsAgainst          string `json:"ck_allowed"`
+	OwnGoals                    string `json:"own_goals"`
+	PostShotxG                  string `json:"ps_xG"`
+	PostShotxGSot               string `json:"ps_xG_SoT"`
+	PostShotxGMinusGlsAllowed   string `json:"ps_xG_minus_gls_allowed"`
+	PostShotxGMinusGlsAllowedFt string `json:"ps_xG_minus_gls_allowed_ft"`
+
+	// launched more than 40yards
+	LaunchedPassesCompletedLonger string `json:"passes_cmp_longer"`
+	LaunchedPassesAttemptedLonger string `json:"passes_att_longer"`
+	LaunchedPassesCompletedPer    string `json:"passes_cmp_per"`
+
+	// passes not including goal kick
+	PassesAttempted string `json:"passes_att"`
+	ThrowsAttempted string `json:"thr_att"`
+	LaunchPer       string `json:"launch_per"`
+	AverageLen      string `json:"avg_len"`
+
+	// goal kicks
+	GkAttempted  string `json:"gk_att"`
+	GkLaunchPer  string `json:"gk_launch_per"`
+	GkAverageLen string `json:"gk_avg_len"`
+
+	// Crosses
+	OpponentAttemptedCrosses string `json:"crosses_opp"`
+	CrossesStopedByKeeper    string `json:"crosses_stp_by_keeper"`
+	CrossesStopedByKeeperPer string `json:"crosses_stp_by_keeper_per"`
+
+	// sweeper
+	NDefensiveActionsByKeeper   string `json:"n_defense_actions_by_keeper"`
+	NDefensiveActionsByKeeperFt string `json:"n_defense_actions_by_keeper_ft"`
+	AverageDistance             string `json:"average_distance"`
+}
+
+// TODO: maybe create a interface for a player, to solve the problem where i need to get the stats from diff tables
+// TODO: create a struct to handle all players statistics
+type PlayerBasic struct {
+	Name   string `json:"name"`
+	Nation string `json:"nation"`
+	Pos    string `json:"position"`
+	Age    string `json:"age"`
+	Min    string `json:"minutes_played"`
+}
+
+type Player struct {
+	PlayerBasicInfo  PlayerBasic
+	Shooting         ShootingStats         `json: "shooting_stats"`
+	Passing          PassingStats          `json:"passing_stats"`
+	PassType         PassTypesStats        `json:"passing_types"`
+	GoalShotCreation GoalShotCreationStats `json:"goal_shot_creation"`
+	DefenseActions   DefenseStats          `json:"defense_actions"`
+	Possession       PossessionStats       `json:"possession"`
+	PlayingTime      PlayingTimeStats      `json:"playing_time"`
+	Miscellaneous    MiscellaneousStats    `json:"miscellaneous"`
+	PlayerSummary    PlayerSummaryStats    `json:"player_summary"`
+	Goalkeeping      GoalKeepingStats      `json:"goalkeeping"`
+}
