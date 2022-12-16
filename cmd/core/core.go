a522a8baf0de7bd3627bf0900e00d57e4225133b
Victor Milhomem <victormllosada@gmail.com>
2022-12-15 22:56:21 -0300

    goalkeeping stats scrape working

--- cmd/core/core.go
+++ cmd/core/core.go
@@ -13,6 +13,56 @@ var players []types.Player
 
 // TODO: create functions to get all players statistics
 
+func getGkAdvancedStats(c *colly.Collector) error {
+	var gkStats types.GoalKeepingStats
+
+	c.OnHTML("table#stats_keeper_adv_combined > tbody", func(h *colly.HTMLElement) {
+		h.ForEach("tr", func(i int, el *colly.HTMLElement) {
+			gkStats = types.GoalKeepingStats{
+				GoalAgainst:                 el.ChildText("td:nth-child(6)"),
+				PenaltyKickAllowed:          el.ChildText("td:nth-child(7)"),
+				FreeKickAllowed:             el.ChildText("td:nth-child(8)"),
+				CornerGoalsAgainst:          el.ChildText("td:nth-child(9)"),
+				OwnGoals:                    el.ChildText("td:nth-child(10)"),
+				PostShotxG:                  el.ChildText("td:nth-child(11)"),
+				PostShotxGSot:               el.ChildText("td:nth-child(12)"),
+				PostShotxGMinusGlsAllowed:   el.ChildText("td:nth-child(13)"),
+				PostShotxGMinusGlsAllowedFt: el.ChildText("td:nth-child(14)"),
+
+				// launched more than 40yards
+				LaunchedPassesCompletedLonger: el.ChildText("td:nth-child(15)"),
+				LaunchedPassesAttemptedLonger: el.ChildText("td:nth-child(16)"),
+				LaunchedPassesCompletedPer:    el.ChildText("td:nth-child(17)"),
+
+				// passes not including goal kick
+				PassesAttempted: el.ChildText("td:nth-child(18)"),
+				ThrowsAttempted: el.ChildText("td:nth-child(19)"),
+				LaunchPer:       el.ChildText("td:nth-child(20)"),
+				AverageLen:      el.ChildText("td:nth-child(21)"),
+
+				// goal kicks
+				GkAttempted:  el.ChildText("td:nth-child(22)"),
+				GkLaunchPer:  el.ChildText("td:nth-child(23)"),
+				GkAverageLen: el.ChildText("td:nth-child(24)"),
+
+				// Crosses
+				OpponentAttemptedCrosses: el.ChildText("td:nth-child(25)"),
+				CrossesStopedByKeeper:    el.ChildText("td:nth-child(26)"),
+				CrossesStopedByKeeperPer: el.ChildText("td:nth-child(27)"),
+
+				// sweeper
+				NDefensiveActionsByKeeper:   el.ChildText("td:nth-child(28)"),
+				NDefensiveActionsByKeeperFt: el.ChildText("td:nth-child(29)"),
+				AverageDistance:             el.ChildText("td:nth-child(30)"),
+			}
+
+			players[i].Goalkeeping = gkStats
+		})
+	})
+
+	return nil
+}
+
 func getPlayersShootingStats(c *colly.Collector) error {
 	var (
 		playerInfo    types.PlayerBasic
@@ -60,13 +110,18 @@ func getPlayersShootingStats(c *colly.Collector) error {
 	return nil
 }
 
+func checkError(err error) {
+	if err != nil {
+		log.Fatal("Error getting players statistics: ", err)
+	}
+}
+
 func ScrapePlayers(url string, c *colly.Collector) {
 	// TODO: getting the struct field infos from each respective table
 
 	err := getPlayersShootingStats(c)
-	if err != nil {
-		log.Fatal("Error getting players shooting statistics: ", err)
-	}
+	checkError(err)
+	err = getGkAdvancedStats(c)
 
 	c.OnScraped(func(r *colly.Response) {
 		enc := json.NewEncoder(os.Stdout)
